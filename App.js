ba753872247f6d08e9aacc3e217c57769544aaab
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-01-17 20:04:22 +0000

    Feat: toggle color scheme added

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useColorScheme } from 'nativewind';
+import { StyleSheet, Switch, Text, View } from 'react-native';
 
 export default function App() {
+  const {colorScheme, toggleColorScheme} = useColorScheme();
   return (
-    <View style={styles.container}>
+    <View className="flex-1 justify-center items-center bg-white">
+      <View className="flex-row justify-center items-center space-x-2">
+        <Text className="text-xl">Toggle Theme</Text>
+        <Switch value={colorScheme=='dark'} onChange={toggleColorScheme} />
+      </View>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
