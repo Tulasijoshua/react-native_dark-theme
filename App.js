d9ecda96ce1277b967d9b8afd89b8df001c4aaba
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-01-17 20:07:19 +0000

    Feat: dark mode

--- App.js
+++ App.js
@@ -5,12 +5,14 @@ import { StyleSheet, Switch, Text, View } from 'react-native';
 export default function App() {
   const {colorScheme, toggleColorScheme} = useColorScheme();
   return (
-    <View className="flex-1 justify-center items-center bg-white">
+    <View className="flex-1 justify-center items-center bg-white dark:bg-neutral-900">
       <View className="flex-row justify-center items-center space-x-2">
-        <Text className="text-xl">Toggle Theme</Text>
+        <Text className="text-xl dark:text-white">Toggle Theme</Text>
         <Switch value={colorScheme=='dark'} onChange={toggleColorScheme} />
       </View>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        lorem23
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
